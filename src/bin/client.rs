c3e463cbd92cad18f935f3c7c5b65a155e86ed7c
ZaneHisako <yassinetakiko@gmail.com>
2024-08-28 05:46:53 +0100

    its working

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,3 +1,5 @@
+use std::io::Read;
+
 use tokio::{io, net::UdpSocket};
 
 #[tokio::main]
@@ -7,12 +9,10 @@ async fn main() -> io::Result<()> {
     let mut buffer = [0; 1024];
     let addr = "127.0.0.1:8080";
     loop {
-        let mut msg = String::new();
-        std::io::stdin().read_line(&mut msg)?;
+        std::io::stdin().read(&mut buffer)?;
         //Since UDP is connectionless, the socket doesn't keep track of who it's sending data to—you must specify the address each time
-        socket.send_to(msg.as_bytes(), addr).await?;
+        socket.send_to(&buffer[..3], addr).await?;
         let (len, _) = socket.recv_from(&mut buffer).await?;
         println!("{:?} ", String::from_utf8_lossy(&buffer[..len]));
     }
-    Ok(())
 }
--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,21 +1,30 @@
+use rand::Rng;
 use tokio::{io, net::UdpSocket};
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
     //127.0.0.1 is the IP address, which typically refers to the local machine (localhost).
     let socket = UdpSocket::bind("127.0.0.1:8080").await?;
-    let mut buffer = [0; 1024];
+    let mut buffer = [0; 4];
+    let key = rand::thread_rng().gen_range(0..1000);
+    println!("the key {:?}", key);
+
     loop {
         //This method waits for data to arrive and then returns the data along with the sender's address
         let (len, addr) = socket.recv_from(&mut buffer).await?;
-        println!(
-            "{:?} \ncoming from{:?}",
-            String::from_utf8_lossy(&buffer[..len]),
-            addr
-        );
+        let msg: u32 = String::from_utf8_lossy(&buffer[..len])
+            .trim()
+            .parse()
+            .unwrap();
+
+        println!("{:?} coming from {:?}", msg, addr);
+
+        if msg == key {
+            socket.send_to(b"Congratulations ", addr).await?;
+        } else {
+            socket.send_to(b"wrong try again", addr).await?;
+        }
 
         //Since UDP is connectionless, the socket doesn't keep track of who it's sending data to—you must specify the address each time
-        let len = socket.send_to(b"message recived", addr).await?;
     }
-    Ok(())
 }
